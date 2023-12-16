9eda210f956b48e186a77c505996473723fa68df
Tonari239 <vanina103@abv.bg>
2023-12-17 01:31:59 +0200

    Add initial files for client and simple server.

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        public static List<ServerClient> clients;
+        public static string hostName = ConfigurationManager.AppSettings.Get("hostName");
+        public static int port = int.Parse(ConfigurationManager.AppSettings.Get("portNumber"));
+
+        public static int Main(String[] args)
+        {
+           
+            InitializeClients();
+            
+            SendDataFromClients();
+
+            CleanUp();
+            return 0;
+        }
+
+        public static void InitializeClients()
+        {
+            Console.WriteLine("Enter how many clients you'd like to have.");
+            int clientsCount;
+            clientsCount = int.Parse(Console.ReadLine());
+            clients = new List<ServerClient>();
+            for (int i = 0; i < clientsCount; i++)
+            {
+                clients.Add(new ServerClient(hostName, port));
+                clients[i].ConnectToServer();
+            }
+        }
+
+        public static void SendDataFromClients()
+        {
+            clients.ForEach(x=>x.SendData("Holy macaroni<EOF>"));
+        }
+
+        public static void CleanUp()
+        {
+            clients.ForEach(x => x.CloseServerConnection());
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Client/ServerClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class ServerClient
+    {
+        private readonly Socket _serverConnection;
+        private readonly IPEndPoint _remoteEndPoint;
+        private byte[] buffer = new byte[1024];
+
+        private static int clientId = 0;
+        public ServerClient(string serverName, int port)
+        {
+            IPAddress _serverIpAddress = Dns.GetHostEntry(serverName).AddressList[0];
+            _remoteEndPoint = new IPEndPoint(_serverIpAddress, port);
+            _serverConnection = new Socket(_serverIpAddress.AddressFamily,
+                   SocketType.Stream, ProtocolType.Tcp);
+            clientId++;
+        }
+
+        public void ConnectToServer()
+        {
+            _serverConnection.Connect(_remoteEndPoint);
+
+            Console.WriteLine($"Socket with id {clientId} connected to server.");
+        }
+
+        public void SendData(dynamic data)
+        {
+            try
+            {
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                _serverConnection.Send(msg);
+
+                int bytesRec = _serverConnection.Receive(buffer);
+                Console.WriteLine("Echoed test = {0}",
+                    Encoding.ASCII.GetString(buffer, 0, bytesRec));
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+
+        public void CloseServerConnection()
+        {
+            _serverConnection.Shutdown(SocketShutdown.Both);
+            _serverConnection.Close();
+        }
+    }
+}
